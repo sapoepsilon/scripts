f583cbe5f4d9cd071f3c74e8fd74cbe2b55ac707
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-09-06 23:39:32 -0600

    feat/ Arch Linux option

--- installOhMyZSH.sh
+++ installOhMyZSH.sh
@@ -3,8 +3,8 @@
 echo "What is your OS? (Enter 1 for macOS, 2 for Ubuntu, or 3 for Fedora)"
 read os
 
-while [ "$os" != "1" ] && [ "$os" != "2" ] && [ "$os" != "3" ]; do
-    echo "Invalid input. Please enter 1 for macOS, 2 for Ubuntu, or 3 for Fedora."
+while [ "$os" != "1" ] && [ "$os" != "2" ] && [ "$os" != "3" ] && [ "$os" != "4" ]; do
+    echo "Invalid input. Please enter 1 for macOS, 2 for Ubuntu, 3 for Fedora, or 4 for Arch Linux."
     read os
 done
 
@@ -109,6 +109,7 @@ elif [ $os = "2" ]; then
 
 elif [ $os = "3" ]; then
 
+
     # code for Fedora
     echo "Checking for git..."
     if ! [ -x "$(command -v git)" ]; then
@@ -144,6 +145,57 @@ elif [ $os = "3" ]; then
         sed -i -e 's/plugins=(git)/plugins=(git jump zsh-autosuggestions sublime zsh-history-substring-search jsontools zsh-syntax-highlighting zsh-interactive-cd)/g' ~/.zshrc
         echo "Please restart your terminal for changes to take effect"
     fi
+
+elif [ $os = "4" ]; then
+    echo "You have selected Arch Linux"
+    
+    # Verificar si git está instalado
+    if ! command -v git &> /dev/null; then
+        echo "Git not found. Installing..."
+        sudo pacman -S --noconfirm git
+    else
+        echo "Git found."
+    fi
+
+    # Instalar fzf
+    echo "Installing fzf"
+    sudo pacman -S --noconfirm fzf
+
+    # Instalar ZSH si no está instalado
+    if ! command -v zsh &> /dev/null; then
+        echo "Installing ZSH"
+        sudo pacman -S --noconfirm zsh
+    else
+        echo "ZSH already installed."
+    fi
+
+    # Cambiar el shell predeterminado a ZSH
+    echo "Setting ZSH as default shell"
+    chsh -s $(which zsh)
+    echo "Your default shell is now " 
+    echo $SHELL
+
+    # Instalar Oh My Zsh
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "Installing Oh My Zsh"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    else
+        echo "Oh My Zsh already installed"
+    fi
+
+    # Instalar Powerlevel10k
+    echo "Installing Powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+    # Instalar plugins
+    echo "Installing plugins"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    sed -i 's/plugins=(git)/plugins=(git jump zsh-autosuggestions zsh-history-substring-search jsontools zsh-syntax-highlighting zsh-interactive-cd)/g' ~/.zshrc
+
+    echo "Please restart your terminal for changes to take effect"
 
 else
     echo "Error installing Oh My Zsh"
